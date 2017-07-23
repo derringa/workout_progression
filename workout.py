92718fbfd39736aba82e6bd698d95251cea91807
Andrew <andy.derringer@live.com>
2017-07-22 19:36:08 -0700

    Update workout.py

--- workout.py
+++ workout.py
@@ -1,11 +1,11 @@
 print ('Weekly Workout Progression')
 	
 max_lift = {'Squat': 300, 'Bench': 265, 'Deadlift': 300, 'Militry': 155}
-#These are the maxes you can actually lift placed near the top for easy change
+#These are the maxes you can actually lift placed near the top for easy change.
 
 for value in max_lift:
 	max_lift[value] *= 0.95
-	# We calculated all sets using 95% of our actual max
+	# We calculated all sets using 95% of our actual max.
 	
 def week_1():
 	for value in max_lift:
@@ -13,6 +13,7 @@ def week_1():
 		print ('5 X %d' % (max_lift[value] * 0.65))
 		print ('5 X %d' % (max_lift[value] * 0.75))
 		print ('5 X %d' % (max_lift[value] * 0.85))
+		#Week 1 is 3 sets of 5 at 65%, 75%, and 85% of revised max.
 		
 def week_2():
 	for value in max_lift:
@@ -20,6 +21,7 @@ def week_2():
 		print ('3 X %d' % (max_lift[value] * 0.70))
 		print ('3 X %d' % (max_lift[value] * 0.80))
 		print ('3 X %d' % (max_lift[value] * 0.90))
+		#Week 2 is 3 sets of 3 at 70%, 80%, and 90% of revised max.
 	
 def week_3():
 	for value in max_lift:
@@ -27,6 +29,7 @@ def week_3():
 		print ('5 X %d' % (max_lift[value] * 0.75))
 		print ('3 X %d' % (max_lift[value] * 0.85))
 		print ('1 X %d' % (max_lift[value] * 0.95))
+		#Week 3 is 3 sets of 5/3/1 at 75%, 85%, and 95% of revised max.
 		
 def week_4():
 	for value in max_lift:
@@ -34,6 +37,7 @@ def week_4():
 		print ('5 X %d' % (max_lift[value] * 0.60))
 		print ('5 X %d' % (max_lift[value] * 0.60))
 		print ('5 X %d' % (max_lift[value] * 0.60))
+		#Week 4 is 3 sets of 5 deloaded back to 60% of revised max.
 
 print ('Week 1 3X5')
 print (week_1())
